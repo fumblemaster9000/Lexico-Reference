6339298b64156ff7584c1dca28473c1854e4f722
fumblemaster9000 <kylebuchanek@gmail.com>
2026-07-26 17:40:45 -0400

    Parser now supports PDF, TXT, and DOCX

--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,27 @@
+import os
 #PDF reader function
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    Docx2txtLoader,
+    TextLoader
+    )
 
-def read_pdf(file_path: str): #function definition
-    pdf_loader = PyPDFLoader(file_path) #Tool initilization
-    raw_documents = pdf_loader.load()
+def read_file(file_path: str):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
     
-    print(f"Succesfully read {len(raw_documents)} pages from the PDF.")
-    return raw_documents
+    #isolate the extension of the file [all lower case]
+    extension = os.path.splitext(file_path)[1].lower()
+    
+    if extension == ".pdf":
+        loader = PyPDFLoader(file_path)
+    elif extension == ".docx":
+        loader = Docx2txtLoader(file_path)
+    elif extension in [".txt", ".md"]:
+        loader = TextLoader(file_path, encoding="utf-8")
+    else:
+        raise ValueError(f"Document extension {extension} is not recognized.")
+    
+    documents = loader.load()
+    print(f"Successfully parsed {len(documents)} page(s) from {file_path}")
+    return documents
--- src/testrunner.py
+++ src/testrunner.py
@@ -1,10 +1,24 @@
-from parser import read_pdf
+from parser import read_file
 
 if __name__ == "__main__":
+    
+    #pdf test
     test_pdf_path = "data/sample.pdf"
-    
-    docs = read_pdf(test_pdf_path)
-    
+    docs = read_file(test_pdf_path)
     if docs:
         print("\n|Read and Check Data|")
         print(docs[0].page_content[:300]) # Prints the first 300 characters
+    
+    #docx test
+    test_pdf_path = "data/file-sample_100kB.docx"
+    docs = read_file(test_pdf_path)
+    if docs:
+            print("\n|Read and Check Data|")
+            print(docs[0].page_content[:300]) # Prints the first 300 characters
+            
+    #txt test
+    test_pdf_path = "data/example.txt"
+    docs = read_file(test_pdf_path)
+    if docs:
+            print("\n|Read and Check Data|")
+            print(docs[0].page_content[:300]) # Prints the first 300 characters
